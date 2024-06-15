61da00e8b3c439f9a417afe76de39368def1a207
soori <sooribalan@outlok.com>
2024-06-15 20:00:11 +0530

    Edited provider.tf

--- provider.tf
+++ provider.tf
@@ -16,7 +16,10 @@ terraform {
 }
 
 provider "azurerm" {
-  features {
+  features {}
 
-  }
+  subscription_id   = "736f8140-d7a6-40ec-86b4-9e3da5fc8382"
+  tenant_id         = "06155f75-47a2-44dd-9324-7f35f30a5fbe"
+  client_id         = "25ba940b-1190-408f-9e70-61005e853c66"
+  client_secret     = "P5w8Q~QDnUBbFSzdOAVcyq4HnYdq6Tkvn6Rq5cqK"
 }
