72aa661f6e428307bb32ab7d123e3bb8c6faabc3
sooribalan <166509355+sooribalan@users.noreply.github.com>
2024-06-15 21:57:10 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -5,12 +5,6 @@ terraform {
       version = "3.107.0"
     }
   }
- backend "azurerm" {
-    resource_group_name  = "jenkins"  
-    storage_account_name = "aksstorage7305"                      
-    container_name       = "tfstate"                       
-    key                  = "prod.terraform.tfstate"
-  }
 }
 
 provider "azurerm" {
