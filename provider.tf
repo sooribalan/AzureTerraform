0c6ba29ab1f2781616e726a3ff1f6d44c7fb6cb5
soori <sooribalan@outlok.com>
2024-06-15 16:12:02 +0530

    Added backend

--- provider.tf
+++ provider.tf
@@ -1,4 +1,11 @@
 terraform {
+
+  backend "azurerm" {
+    resource_group_name  = "jenkins"  
+    storage_account_name = "aksstorage7305"                      
+    container_name       = "tfstate"                       
+    key                  = "dev.terraform.tfstate"        
+  }
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
