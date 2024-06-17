d759c7aeb4893b317c4cbf016f9a9fea1526c9be
sooribalan <166509355+sooribalan@users.noreply.github.com>
2024-06-17 20:15:17 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -3,11 +3,8 @@ terraform {
     resource_group_name  = "jenkins"          # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
     storage_account_name = "aksstorage7305"                              # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
     container_name       = "tfstate"                               # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
-    key                  = "terraform.tfstate"                # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
-    use_msi              = true                                   # Can also be set via `ARM_USE_OIDC` environment variable.
-    client_id            = "f8229643-6603-4ace-b3f7-ff2bef82267a"  # Can also be set via `ARM_CLIENT_ID` environment variable.
-    subscription_id      = "736f8140-d7a6-40ec-86b4-9e3da5fc8382"  # Can also be set via `ARM_SUBSCRIPTION_ID` environment variable.
-    tenant_id            = "06155f75-47a2-44dd-9324-7f35f30a5fbe"  # Can also be set via `ARM_TENANT_ID` environment variable.
+    key                  = "prod.terraform.tfstate"                   # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+    sas_token            = "sv=2022-11-02&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2024-06-17T22:43:59Z&st=2024-06-17T14:43:59Z&spr=https&sig=I6bBpzEGtyz1vIpcnolD4rxeUZpXPcQw9Sgbr%2FjaZEA%3D"  # Can also be set via `ARM_TENANT_ID` environment variable.
   }
 
   required_providers {
