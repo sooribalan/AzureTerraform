cf3571e44f84e12e3162e4cac0b1d884f17122b4
sooribalan <166509355+sooribalan@users.noreply.github.com>
2024-06-17 19:14:57 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,7 +9,6 @@ terraform {
     subscription_id      = "736f8140-d7a6-40ec-86b4-9e3da5fc8382"  # Can also be set via `ARM_SUBSCRIPTION_ID` environment variable.
     tenant_id            = "06155f75-47a2-44dd-9324-7f35f30a5fbe"  # Can also be set via `ARM_TENANT_ID` environment variable.
   }
-}
 
   required_providers {
     azurerm = {
