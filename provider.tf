bbc298f0a2a635222aba4957810343a9e9f5ba24
sooribalan <166509355+sooribalan@users.noreply.github.com>
2024-06-17 18:57:42 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,11 +1,5 @@
 terraform {
 
-backend "azurerm" {
-    resource_group_name  = "jenkins"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
-    storage_account_name = "aksstorage7305"                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
-    container_name       = "tfstate"                       # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
-    key                  = "dev.terraform.tfstate"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
-  }
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
