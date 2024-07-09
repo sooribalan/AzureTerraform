08abf181ab3aebdcab18b1d4f0abee3ef9bd595a
sooribalan <166509355+sooribalan@users.noreply.github.com>
2024-07-09 19:18:36 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,15 +7,3 @@ variable "location" {
   type        = string
   description = "Location of the resources to be created"
 }
-
-variable "service_principal_name" {
-  type = string
-}
-
-variable "keyvault_name" {
-  type = string
-}
-
-variable "stateRG" {
-  type = string
-}
